fcc0b3f8dcaa38ae3cdd44a9f796c1114843baa6
Joletuar <96794364+Joletuar@users.noreply.github.com>
2023-02-01 14:15:51 +0000

    Avance 2

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.pages, name='pages'),
-    path('<int:page_id>/<slug:page_slug>/', views.page, name='page'),
+    path('', views.PageListView.as_view(), name='pages'),
+    path('<int:pk>/<slug:slug>/', views.PageDetailView.as_view(), name='page'), # El modelo espera el id con el nombre pk
+    path('create/', views.PageCreateView.as_view(), name='create_page')
 ]
+
+# Existe otra forma que declarar las urls
+"""
+pages_patterns = ([
+    path('', views.PageListView.as_view(), name='pages'),
+    path('<int:pk>/<slug:slug>/', views.PageDetailView.as_view(), name='page'),
+    path('create/', views.PageCreateView.as_view(), name='page_create')
+], 'pages')
+"""
+
+# Al realizar esto debemos de tambien cambiar el modo en que se importan las urls dentro del archivo urls.py del proyecto principal.
+# Con esto realizado ahora al momento de referencia un url dentro de los templates se debe realizarlo de la siguiente forma:
+# pages:pages, pages:page, pages:create
