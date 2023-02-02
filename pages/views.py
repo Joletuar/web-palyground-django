2a6a7ea428e3238d15d4e4f9862d2c1f58eba41b
Joletuar <96794364+Joletuar@users.noreply.github.com>
2023-02-02 13:45:03 +0000

    Avances trabajo

--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,32 @@
 from .models import Page
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView # Clase modelo que se utiliza para recorrer modelos y mostrarlos en template
 from django.views.generic.detail import DetailView # Clase modelo que se utiliza para mostrar el detalle en un template
 from django.views.generic.edit import CreateView # Clase modelo que se utiliza para mostrar un template de creación
 from django.views.generic.edit import UpdateView # Clase modelo que se utiliza para mostrar el template de actualización
 from django.views.generic.edit import DeleteView # Clase modelo que se utiliza para mostrar el template de eliminación
-from .forms import *
+from .forms import PageForm
+from django.shortcuts import redirect
+from django.contrib.admin.views.decorators import staff_member_required # Importamos el decorador que verifica que el usuario sea miembro de l staff
+from django.utils.decorators import method_decorator # Decorador que permite decorar métodos
 
-
-
+# Diferencia entre reverse y reverse_lazy: https://es.stackoverflow.com/questions/258616/reverse-vs-reverse-lazy-django
 # Dentro de los templates se puede acceder con "object" o con el "nombre del modelo"
+# Un mixin es una implementación de una o varias funcionalidades para una clase
+
+class StaffRequiredMixin(object):
+    # Este mixin requerirá que el usuario sea miembro del staff
+    
+    # Sobreescribir este método es una alternativa para verificar que el miembro que intente ingresar a esta vista sea parte del staff
+    def dispatch(self, request, *args, **kwargs):
+        # Verificamos si el usuario forma parte del staff
+        if not request.user.is_staff:
+            return redirect(reverse_lazy('admin:login'))
+        return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
+
+    # Posterior a esto procedemos a heredar en las clases que se requieran
+    # Se elimina el mixin debido a que hora usaremos un decorador
+
 
 # Al template hay que cambiarle el nombre y ponerle page_list
 class PageListView(ListView):
@@ -20,6 +37,10 @@ class PageListView(ListView):
 class PageDetailView(DetailView):
     model = Page
 
+# --- Vistas para nuestro CRUD --- #
+
+# --- Vista de creación
+@method_decorator(staff_member_required, name='dispatch')
 class PageCreateView(CreateView):
     model = Page
     
@@ -37,6 +58,8 @@ class PageCreateView(CreateView):
     # Método más facil que sobreescribir el método anterior
     success_url = reverse_lazy('pages')
 
+# --- Vista de actualización
+@method_decorator(staff_member_required, name='dispatch')
 class PageUpdateView(UpdateView):
     model = Page
     
@@ -53,6 +76,8 @@ class PageUpdateView(UpdateView):
         # Se concatena un 'ok' para comprobar si la actualización se realizó correctamente
         return reverse_lazy('update_page', args=[self.object.id]) + '?ok'
 
+# --- Vista de eliminación
+@method_decorator(staff_member_required, name='dispatch')
 class PageDeleteView(DeleteView):
     model = Page
     success_url = reverse_lazy('pages')
